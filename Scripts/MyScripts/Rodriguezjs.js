d913d8eb6cb72653162e827471cc1cb95b74f4a3
oscar0392 <slager_92@hotmail.com>
2014-11-28 00:05:32 -0600

    mi primer comit

---
+++ Scripts/MyScripts/Rodriguezjs.js
@@ -0,0 +1,9 @@
+﻿function acceso()
+{
+    while (prompt("Ingresa La Contraseña") != "TECNOLOGICO GAM") {
+        alert("Aceeso Denegado");
+
+    }
+    alert("Gracias Disfruta el Programa");
+
+}
